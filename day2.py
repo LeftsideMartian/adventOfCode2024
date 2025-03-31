9c97aec23c1560a919a91d8fc0737c998ff19641
Matt <matthew@adler.id.au>
2025-03-31 13:56:00 +1100

    Day 2 complete - I hate python D:

---
+++ day2.py
@@ -0,0 +1,65 @@
+def getReports(fileName):
+    reports = []
+
+    with open(fileName, "r") as f:
+        for line in f:
+            reports.append(line.strip("\n").split(" "))
+        f.close()
+
+    return reports
+
+def isReportSafe(report):
+    isAscending = True
+    isDescending = True
+
+    for i in range(1, len(report)):
+        thisItem = int(report[i])
+        lastItem = int(report[i-1])
+        itemDistance = abs(thisItem - lastItem)
+
+        if isAscending and lastItem >= thisItem:
+            isAscending = False
+        if isDescending and lastItem <= thisItem:
+            isDescending = False
+        if itemDistance < 1 or itemDistance > 3:
+            return False
+        if not isAscending and not isDescending:
+            return False
+    
+    return True
+
+def part1(reports):
+    numOfSafeReports = 0
+    unsafeReports = []
+
+    for report in reports:
+        if isReportSafe(report):
+            numOfSafeReports += 1
+        else:
+            unsafeReports.append(report)
+    
+    return (unsafeReports, numOfSafeReports)
+
+def part2(reports):
+    numOfSafeReports = 0
+
+    for report in reports:
+        for i in range(len(report)):
+            newReport = report.copy()
+            newReport.pop(i)
+            if isReportSafe(newReport):
+                numOfSafeReports += 1
+                break
+
+    return numOfSafeReports
+
+myInput = "day2.txt"
+testInput = "day2TestInput.txt"
+
+reports = getReports(myInput)
+
+unsafeReports, safeReports = part1(reports)
+print(f"Part 1: Number of safe reports is {safeReports}")
+
+safeReports += part2(unsafeReports)
+print(f"Part 2: Number of safe reports is {safeReports}")
